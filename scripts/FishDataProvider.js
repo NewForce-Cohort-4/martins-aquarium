7f261bce22a7c9579ecfad5be9e26e15724ed212
Tommy Spurlock <tspurloc@gmail.com>
2021-01-13 09:56:18 -0500

    added FishDataProvider.js

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,49 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+    {
+        name: "Bart",
+        food: "crustaceans",
+        length: "57cm",
+        locationHarvested: "creek out back",
+        typeOfFish: "Beta"
+
+    },
+    {
+        name: "Poppy",
+        food: "seeds",
+        length: "100cm",
+        locationHarvested: "Ohio River",
+        typeOfFish: "Sun Fish"
+
+    },
+    {
+        name: "Sam",
+        food: "other fish",
+        length: "68cm",
+        locationHarvested: "South of France",
+        typeOfFish: "Clown"
+
+    },
+    {
+        name: "Marney",
+        food: "Jelly Fish",
+        length: "307cm",
+        locationHarvested: "Atlantic",
+        typeOfFish: "Big Fish"
+
+    },
+    {
+        name: "Bruce",
+        food: "Anything it wants ",
+        length: "1020cm",
+        locationHarvested: "Pacific Ocean",
+        typeOfFish: "Great White Shark"
+
+    }
+
+]
